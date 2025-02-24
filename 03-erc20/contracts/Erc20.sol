242326d84af40276ce163efe06be0cc1c5f541aa
Angela-Mae-Cal <angela.mae.cal@adamson.edu.ph>
2025-02-24 15:27:32 +0800

    Calii

--- 03-erc20/contracts/Erc20.sol
+++ 03-erc20/contracts/Erc20.sol
@@ -1,13 +1,13 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NameYourToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("NameYourToken", "NYT") {
-        // _mint(msg.sender, initialSupply);
+contract CalToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TokToken", "Cali") {
+        _mint(msg.sender, initialSupply);
     }
 
     function generateTokens(address destination, uint256 amount) public {
-        // _mint(destination, amount);s
+        _mint(destination, amount);
     }
-}
+} 
